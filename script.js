418f4d92e8ea9eca01d4f95257f0f422e7fca090
Adelina Moroaca <moroacaadelina@gmail.com>
2023-12-06 23:29:58 +0200

    Add button that changes the pixels number on table when user request this

--- script.js
+++ script.js
@@ -1,29 +1,69 @@
 const container = document.getElementById('container');
-//let drawTable = document.createElement('div');
+const drawTable = document.createElement('div');
 let square = document.createElement('div');
+let button = document.getElementById('pixel');
+
+container.style.border = '2px solid red';
+container.style.backgroundColor = 'purple';
+container.style.width = '300px';
+container.style.height = '300px';
+container.style.display = 'flex';
+container.style.justifyContent = 'center';
+container.style.alignItems = 'center';
+
+
+drawTable.style.display = 'flex';
+drawTable.style.flexFlow = 'row wrap';
+drawTable.style.backgroundColor = 'pink';
+drawTable.style.border = '2px solid yellow';
+drawTable.style.width = '256px';
+drawTable.style.height = '256px';
+
+container.appendChild(drawTable)
 
 const gridTable = (num) => {
-    container.style.border = '2px solid red';
-    container.style.width = '160px';
-    container.style.height = '160px';
-    container.style.display = 'flex';
-    container.style.flexFlow = 'row wrap';
-    container.style.justifyContent = 'center';
-    container.style.alignItems = 'center';
     for (i=0; i < num; i++) {
         for (j=0; j < num; j++) {
             let square = document.createElement('div');
-            square.style.margin = '0';
-            square.style.backgroundColor = 'pink';
-            square.style.height = '10px';
-            square.style.width = '10px';
-            container.appendChild(square); 
+            square.style.backgroundColor = 'red';
+            square.style.height = `${256/num}px`;
+            square.style.width = `${256/num}px`;
+            drawTable.appendChild(square); 
             square.addEventListener('mouseenter', function(){
                square.style.backgroundColor = 'blue';
             })
         }
     }
+
 }
 
 gridTable(16)
 
+
+button.addEventListener('click',function(){
+
+    let newDimension = parseInt(prompt("enter the number of squares per side for your new grid"));
+
+    if (newDimension <= 100) {
+        drawTable.textContent= '';
+
+        const gridTable = (newDimension) => {
+            for (i=0; i < newDimension; i++) {
+                for (j=0; j < newDimension; j++) {
+                    let newSquare = document.createElement('div');
+                    newSquare.style.backgroundColor = 'red';
+                    newSquare.style.height = `${256/newDimension}px`;
+                    newSquare.style.width = `${256/newDimension}px`;
+                    drawTable.appendChild(newSquare); 
+                    newSquare.addEventListener('mouseenter', function(){
+                        newSquare.style.backgroundColor = 'blue';
+                    })
+                }
+            }
+        } 
+        gridTable(newDimension)
+    } else {
+        console.log("NOK")
+    }
+
+})
