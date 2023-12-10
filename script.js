554332b0b06f54e9a7d135096c6f548221f444b7
Adelina Moroaca <moroacaadelina@gmail.com>
2023-12-10 23:40:08 +0200

    Add functionality to Color Mode button and Eraser button

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ let buttonColorType = document.getElementById('colorType');
 let buttonRainbow = document.getElementById('rainbowType');
 let buttonErase = document.getElementById('erase');
 let buttonClear = document.getElementById('clear');
-
+let colorPicker =document.getElementById('colorPicker');
+let newColor = 'black';
 
 
 container.style.border = '2px solid red';
@@ -28,19 +29,27 @@ drawTable.style.border = '2px solid yellow';
 drawTable.style.width = '300px';
 drawTable.style.height = '300px';
 
+// square.style.backgroundColor='red';
+
 container.appendChild(drawTable)
 
 const gridTable = (num) => {
     for (i=0; i < num; i++) {
         for (j=0; j < num; j++) {
             let square = document.createElement('div');
-            square.style.backgroundColor = 'red';
+            // let color = document.getElementById('colorPicker').value;
+
+            // square.style.backgroundColor = color;
             square.style.height = `${300/num}px`;
             square.style.width = `${300/num}px`;
             drawTable.appendChild(square); 
             square.addEventListener('mouseenter', function(){
-               square.style.backgroundColor = 'blue';
-            //    square.style.backgroundColor = 'yellow';
+            
+                
+                square.style.backgroundColor = newColor;
+
+            // //    square.style.backgroundColor = 'black';
+            // //    square.style.backgroundColor = 'yellow';
 
             })
         }
@@ -52,144 +61,60 @@ gridTable(16)
 
 outputPixel.textContent = slide.value;
 
-slide.addEventListener('click', function(){
-
+function changePixelNumber(){
     let newDimension = parseInt(slide.value);
     outputPixel.textContent = this.value;
 
     if (newDimension <= 100) {
         drawTable.textContent= '';
-
-        const gridTable = (newDimension) => {
-            for (i=0; i < newDimension; i++) {
-                for (j=0; j < newDimension; j++) {
-                    let newSquare = document.createElement('div');
-                    newSquare.style.backgroundColor = 'red';
-                    newSquare.style.height = `${300/newDimension}px`;
-                    newSquare.style.width = `${300/newDimension}px`;
-                    drawTable.appendChild(newSquare); 
-    
-                    newSquare.addEventListener('mouseenter', function(){
-                        newSquare.style.backgroundColor = 'blue';
-                        // newSquare.addEventListener('mouseover', function(){
-                        //     newSquare.style.backgroundColor = 'blue';
-                        // });
-                    })
-      
-                }
-            }
-        } 
         gridTable(newDimension)
     } else {
         console.log("NOK")
     }
 
-})
+}
+
+
 
 //------------------------------------
+function changeColor(){
+    
+    // document.body.style.backgroundColor = color;
+    // square.style.backgroundColor = color;
+    // drawTable.textContent = '';
+    // square.style.backgroundColor = colorPicker.value;
+    newColor = colorPicker.value;
+    square.style.backgroundColor = newColor;
 
-buttonColorType.addEventListener('click', function(){
+    
 
-    console.log("buttonColorType")
-    let newDimension = parseInt(slide.value);
-    outputPixel.textContent = this.value;
+    console.log(colorPicker.value)
+} 
 
-    if (newDimension <= 100) {
-        drawTable.textContent= '';
 
-        const gridTable = (newDimension) => {
-            for (i=0; i < newDimension; i++) {
-                for (j=0; j < newDimension; j++) {
-                    let newSquare = document.createElement('div');
-                    newSquare.style.backgroundColor = 'red';
-                    newSquare.style.height = `${300/newDimension}px`;
-                    newSquare.style.width = `${300/newDimension}px`;
-                    drawTable.appendChild(newSquare); 
-                    newSquare.addEventListener('mouseenter', function(){
-                        newSquare.style.backgroundColor = 'blue';
-                    })
-                }
-            }
-        } 
-        gridTable(newDimension)
-    } else {
-        console.log("NOK")
-    }
-})
 
-buttonRainbow.addEventListener('click', function(){
-    // window.location=window.location;
-    let newDimensionR = parseInt(buttonRainbow.value);
-    outputPixel.textContent = this.value;
 
-    if (newDimensionR <= 100) {
-        drawTable.textContent= '';
-        console.log("0.2 ~ buttonErase")
+function multicolor(){
+ console.log("color");
+ newColor = 'linear-gradient(' +(Math.random()*360)+'deg,'+getRandomColor()+'0%' + getRandomColor()+' 100%)';
+ square.style.backgroundColor = newColor;
 
-        const gridTable = (newDimensionR) => {
-            for (i=0; i < newDimensionR; i++) {
-                for (j=0; j < newDimensionR; j++) {
-                    let newSquare = document.createElement('div');
-                    newSquare.style.backgroundColor = 'red';
-                    newSquare.style.height = `${300/newDimensionR}px`;
-                    newSquare.style.width = `${300/newDimensionR}px`;
-                    drawTable.appendChild(newSquare); 
-                    newSquare.addEventListener('mouseenter', function(){
-                        // newSquare.style.backgroundColor = 'blue';
-                        // newSquare.style.backgroundColor = 'yellow';
-                        newSquare.style.backgroundColor = 'pink';
+ console.log(colorPicker.value)
+}
 
-                    })
-                }
-            }
-        } 
-        gridTable(newDimensionR)
-    } else {
-        console.log("NOK")
-    }
-})
+function eraseDrawTable(){
+    newColor = drawTable.style.backgroundColor;
+    square.style.backgroundColor = newColor;
+}
 
-buttonErase.addEventListener('click', function(){
-    // console.log("buttonErase")
-    // console.log("0 ~ buttonErase")
-    // console.log("0.1 ~ buttonErase")
-    // console.log("0.2 ~ buttonErase")
-    let newDimensionErase = parseInt(buttonErase.value);
-    console.log("buttonErase",newDimensionErase);
-    outputPixel.textContent = this.value;
-    console.log("0.1 ~ buttonErase");
-
-    // if (newDimension <= 100) {
-    //     drawTable.textContent= outputPixel.textContent;
-    //     console.log("0.2 ~ buttonErase")
-    newSquare.addEventListener('mouseenter', function(){
-        newSquare.style.backgroundColor = 'red';
-        // newSquare.addEventListener('mouseover', function(){
-        //     newSquare.style.backgroundColor = 'blue';
-        // });
-    })
-        const gridTable = (newDimensionErase) => {
-            for (i=0; i < newDimensionErase; i++) {
-                for (j=0; j < newDimensionErase; j++) {
-                    console.log("0 ~ buttonErase")
-
-                    let newSquare = document.createElement('div');
-                    newSquare.style.backgroundColor = 'red';
-                    newSquare.style.height = `${300/newDimension}px`;
-                    newSquare.style.width = `${300/newDimension}px`;
-                    drawTable.appendChild(newSquare); 
-                    newSquare.addEventListener('mouseenter', function(){
-                        newSquare.style.backgroundColor = 'red';
-                    })
-                }
-            }
-        } 
-        gridTable(newDimensionErase)
-    // } else {
-    //     console.log("NOK")
-    // }
-})
-
-buttonClear.addEventListener('click', function(){
+function clear(){
     window.location=window.location;
-})
+}
+
+
+slide.addEventListener('click', changePixelNumber)
+buttonColorType.addEventListener('click', changeColor)
+
+buttonRainbow.addEventListener('click', multicolor)
+buttonErase.addEventListener('click', eraseDrawTable) //
+buttonClear.addEventListener('click', clear)
